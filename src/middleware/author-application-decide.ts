3276eb1c685d86575da29d4b4465782f9ea307d2
cmauf <constantin.mauf@outlook.de>
2022-10-12 10:10:55 +0200

    TLI-62 middleware finished, tests succeed

--- src/middleware/author-application-decide.ts
+++ src/middleware/author-application-decide.ts
@@ -1,6 +1,42 @@
 import { Request, Response } from 'express'
 import { Application } from '../declarations'
+import { NotFound, MethodNotAllowed, BadRequest, GeneralError } from '@feathersjs/errors'
+import checkAuthentication from '../misc/check-authentication'
+import applicationStatuses from '../misc/application-statuses'
 
 export default (app: Application) => (req: Request, res: Response): void => {
-  res.send('Hello World!')
+  const changeApplicationStatus = (): void => {
+    app.service('author-applications').patch(req.params.id, req.body)
+      .then((decidedApplication: any) => {
+        res.send({ decidedApplication })
+      })
+      .catch(() => {
+        res.status(500).send(new GeneralError('something went wrong'))
+      })
+  }
+  const handleApplicationDecision = (): void => {
+    app.service('author-applications').get(req.params.id)
+      .then((application) => {
+        if (application.status !== 'pending') {
+          res.status(400).send(new BadRequest('Application already decided'))
+        } else if (req.body.status === 'withdrawn') { // withdraw application, only possible for applicant
+          checkAuthentication(app, req, res, changeApplicationStatus, 'student', application.applicantId)
+        } else { // decide application, only possible for editor
+          checkAuthentication(app, req, res, changeApplicationStatus, 'editor')
+        }
+      })
+      .catch((err: Error) => { // application not found
+        res.status(404).send(new NotFound(err.message))
+      })
+  }
+
+  if (req.method !== 'PATCH') {
+    res.status(405).send(new MethodNotAllowed('Method not allowed'))
+  } else if (req.body.status === undefined || req.body.decidedOn === undefined) {
+    res.status(400).send(new BadRequest('not all required fields are set'))
+  } else if (!applicationStatuses.includes(req.body.status)) {
+    res.status(400).send(new BadRequest('invalid decision'))
+  } else {
+    handleApplicationDecision()
+  }
 }
--- test/middleware/author-application-decide.test.ts
+++ test/middleware/author-application-decide.test.ts
@@ -32,6 +32,9 @@ describe('authorApplicationDecide middleware', () => {
         userId = user.id
         userToken = await getAuthenticationToken('student@application-decide.com', '123456')
       })
+  })
+
+  beforeEach(async () => {
     await app.service('author-applications').create({
       applicantId: userId,
       areaOfExpertise: 'test',
@@ -42,19 +45,36 @@ describe('authorApplicationDecide middleware', () => {
       applicationId = application.id
     })
   })
-  afterAll(async () => {
-    await app.service('accounts').remove(editorId)
-    await app.service('accounts').remove(userId)
+
+  afterEach(async () => {
     await app.service('author-applications').remove(applicationId)
   })
 
+  afterAll(async () => {
+    await app.service('accounts').remove(editorId)
+    await app.service('accounts').remove(userId)
+  })
+
+  it('should return 405 if http method is not PATCH', async () => {
+    const res = await sendAuthenticatedRequest(app, 'get', `/applications/${applicationId}/decide`, editorToken)
+    expect(res.status).toBe(405)
+  })
+
   it('should return 401 if user is not authenticated', async () => {
-    await request(app).patch(`/applications/${applicationId}/decide`)
+    await request(app)
+      .patch(`/applications/${applicationId}/decide`)
+      .send({
+        status: 'accepted',
+        decidedOn: new Date()
+      })
       .expect(401)
   })
 
   it('should return 403 if user is not an editor', async () => {
-    const res = await sendAuthenticatedRequest(app, 'patch', `/applications/${applicationId}/decide`, userToken)
+    const res = await sendAuthenticatedRequest(app, 'patch', `/applications/${applicationId}/decide`, userToken, {
+      status: 'accepted',
+      decidedOn: new Date()
+    })
     expect(res.status).toBe(403)
   })
 
@@ -83,5 +103,65 @@ describe('authorApplicationDecide middleware', () => {
       status: 'test'
     })
     expect(res.status).toBe(400)
+  })
+
+  it('should return 400 if application is already decided', async () => {
+    await app.service('author-applications').patch(applicationId, {
+      decidedOn: new Date(),
+      status: 'accepted'
+    })
+    const res = await sendAuthenticatedRequest(app, 'patch', `/applications/${applicationId}/decide`, editorToken, {
+      decidedOn: new Date(),
+      status: 'accepted'
+    })
+    expect(res.status).toBe(400)
+  })
+
+  it('should return 403 if status is not "withdrawn" and user is not an editor', async () => {
+    const res = await sendAuthenticatedRequest(app, 'patch', `/applications/${applicationId}/decide`, userToken, {
+      decidedOn: new Date(),
+      status: 'accepted'
+    })
+    expect(res.status).toBe(403)
+  })
+
+  it('should return 403 if status is "withdrawn" and user is not the applicant', async () => {
+    const res = await sendAuthenticatedRequest(app, 'patch', `/applications/${applicationId}/decide`, editorToken, {
+      decidedOn: new Date(),
+      status: 'withdrawn'
+    })
+    expect(res.status).toBe(403)
+  })
+
+  it('should return 200 if status is "withdrawn" and user is the applicant', async () => {
+    const res = await sendAuthenticatedRequest(app, 'patch', `/applications/${applicationId}/decide`, userToken, {
+      decidedOn: new Date(),
+      status: 'withdrawn'
+    })
+    expect(res.status).toBe(200)
+  })
+
+  it('should return 404 if application does not exist', async () => {
+    const res = await sendAuthenticatedRequest(app, 'patch', '/applications/999999/decide', editorToken, {
+      decidedOn: new Date(),
+      status: 'accepted'
+    })
+    expect(res.status).toBe(404)
+  })
+
+  it('should return 200 if status is "accepted" and user is an editor', async () => {
+    const res = await sendAuthenticatedRequest(app, 'patch', `/applications/${applicationId}/decide`, editorToken, {
+      decidedOn: new Date(),
+      status: 'accepted'
+    })
+    expect(res.status).toBe(200)
+  })
+
+  it('should return 200 if status is "rejected" and user is an editor', async () => {
+    const res = await sendAuthenticatedRequest(app, 'patch', `/applications/${applicationId}/decide`, editorToken, {
+      decidedOn: new Date(),
+      status: 'rejected'
+    })
+    expect(res.status).toBe(200)
   })
 })
